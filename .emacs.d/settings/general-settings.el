13c89fbdca19a6db870488aea5ad6d30c087cd4b
Martin Burchell <martinb@aptivate.org>
2014-05-22 13:00:46 +0100

    Fixed to use spaces not tabs globally

--- .emacs.d/settings/general-settings.el
+++ .emacs.d/settings/general-settings.el
@@ -3,6 +3,9 @@
 ; don't show the startup screen
 (setq inhibit-startup-screen 1)
 
+; always use spaces, not tabs, when indenting
+(setq-default indent-tabs-mode nil)
+
 (setq-default fill-column 80)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
