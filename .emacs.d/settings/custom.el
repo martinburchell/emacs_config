1f6304ade84fe2d3cdeaef2bb481a4480b630282
Martin Burchell <martinburchell@spamcop.net>
2014-05-31 17:04:00 +0100

    Set background colour to black

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -21,4 +21,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
