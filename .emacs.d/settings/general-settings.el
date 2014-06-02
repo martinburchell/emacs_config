19dd30c2d20053549efa796a97c08f9076edaa24
Martin Burchell <martinb@aptivate.org>
2014-06-02 12:51:59 +0100

    Added setting to create missing directories when saving new file

--- .emacs.d/settings/general-settings.el
+++ .emacs.d/settings/general-settings.el
@@ -10,4 +10,12 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+; Create any missing directories
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
+                  (make-directory dir t))))))
 (provide 'general-settings)
