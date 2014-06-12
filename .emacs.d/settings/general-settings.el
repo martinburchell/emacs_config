5868965e9df31a9419e289ab387c536390ba8815
Martin Burchell <martinb@aptivate.org>
2014-06-12 11:05:56 +0100

    Added setting to save desktop

--- .emacs.d/settings/general-settings.el
+++ .emacs.d/settings/general-settings.el
@@ -18,4 +18,7 @@
                 (when (and (not (file-exists-p dir))
                            (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
                   (make-directory dir t))))))
+
+(desktop-save-mode 1)
+
 (provide 'general-settings)
