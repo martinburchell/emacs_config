088ad1966841cdbf3104e8aa8a9f410358105bd6
Martin Burchell <martinb@aptivate.org>
2014-11-10 11:44:16 +0000

    Fixed PHP coding standards to operate locally per-buffer

--- .emacs.d/settings/php-settings.el
+++ .emacs.d/settings/php-settings.el
@@ -1,27 +1,26 @@
-(add-hook 'php-mode-hook (lambda ()
-                           (flymake-mode 1)
-                           (c-set-style "linux")
-                           (flymake-phpcs-load)))
-
-(defun setup-wordpress ()
+(defun set-up-wordpress ()
   (php-enable-wordpress-coding-style)
-  (setq flymake-phpcs-standard "WordPress")
+  (set (make-local-variable 'flymake-phpcs-standard) "WordPress")
 )
 
-(defun setup-pear ()
+(defun set-up-pear ()
   (php-enable-pear-coding-style)
-  (setq flymake-phpcs-standard "PEAR")
+  (set (make-local-variable 'flymake-phpcs-standard) "PEAR")
 )
 
-(defun detect-wordpress ()
+(defun set-up-coding-style ()
+  (flymake-mode 1)
+  (c-set-style "linux")
+  (flymake-phpcs-load)
+
   (let ((file (find-file-in-hierarchy (buffer-file-name) "wp-config.php")))
     (if file
-        (setup-wordpress)
-      (setup-pear)
+        (set-up-wordpress)
+      (set-up-pear)
     )
   )
 )
 
-(add-hook 'php-mode-hook 'detect-wordpress)
+(add-hook 'php-mode-hook 'set-up-coding-style)
 
 (provide 'php-settings)
