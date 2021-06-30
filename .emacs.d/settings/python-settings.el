04f3f5c44a92995d5083359bc2422700de7237cb
Martin Burchell <martinburchell@gmail.com>
2021-06-30 17:30:35 +0100

    Remove some unused python settings, jedi complete on dot

--- .emacs.d/settings/python-settings.el
+++ .emacs.d/settings/python-settings.el
@@ -11,79 +11,23 @@
 ; from python.el
 (require 'python)
 
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args (if (system-is-mac)
-				   "--gui=osx --matplotlib=osx --colors=Linux"
-                                   (if (system-is-linux)
-				       "--gui=wx --matplotlib=wx --colors=Linux"))
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-
-
-;; -----------------------------
-;; emacs IPython notebook config
-;; -----------------------------
-
-; use autocompletion, but don't start to autocomplete after a dot
-(setq ein:complete-on-dot -1)
-(setq ein:use-auto-complete 1)
-
-; set python console args
-(setq ein:console-args
-      (if (system-is-mac)
-	  '("--gui=osx" "--matplotlib=osx" "--colors=Linux")
-	(if (system-is-linux)
-	    '("--gui=wx" "--matplotlib=wx" "--colors=Linux"))))
-
-; timeout settings
-(setq ein:query-timeout 1000)
-
-; IPython notebook
-(include-plugin "emacs-ipython-notebook/lisp")
-(require 'ein)
-
-; shortcut function to load notebooklist
-(defun load-ein ()
-  (ein:notebooklist-load)
-  (interactive)
-  (ein:notebooklist-open))
-
-
 ;; ------------------
 ;; misc python config
 ;; ------------------
 
-; pydoc info
-;;(include-plugin "pydoc-info-0.2")
-;;(require 'pydoc-info)
-
 ;; jedi python completion
 (add-hook 'python-mode-hook 'jedi:setup)
-
+(setq jedi:complete-on-dot t)
+;; https://archive.zhimingwang.org/blog/2015-04-26-using-python-3-with-emacs-jedi.html
+(setq jedi:environment-root "jedi")
 (when (load "jedi" t)
   (define-key jedi-mode-map (kbd "<C-tab>") nil))
 
-;; http://www.lunaryorn.com/2013/11/26/syntax-checker-executables-in-flycheck.html
-(defun flycheck-python-set-executables ()
-  (let ((exec-path (python-shell-calculate-exec-path)))
-    (setq flycheck-python-pylint-executable (executable-find "pylint")
-          flycheck-python-flake8-executable (executable-find "flake8")))
-  ;; Force Flycheck mode on
-  (flycheck-mode))
-
 (defun flycheck-python-setup ()
-  (add-hook 'hack-local-variables-hook #'flycheck-python-set-executables
-            nil 'local))
+  (flycheck-mode)
+)
 
 (add-hook 'python-mode-hook #'flycheck-python-setup)
-(add-hook 'python-mode-hook #'flycheck-virtualenv-setup)
 
 (add-hook 'python-mode-hook
   (lambda () (setq python-indent-offset 4)))
