a5dfc1e8bf9b322ae57d69bdd08e059f55e39912
Martin Burchell <martinburchell@gmail.com>
2021-11-05 15:40:44 +0000

    Change ediff faces

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -4,8 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(buffers-menu-max-size 20)
- '(custom-safe-themes (quote ("53c542b560d232436e14619d058f81434d6bbcdc42e00a4db53d2667d841702e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" default)))
- '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".ve")))
+ '(custom-safe-themes
+   (quote
+    ("53c542b560d232436e14619d058f81434d6bbcdc42e00a4db53d2667d841702e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" default)))
+ '(grep-find-ignored-directories
+   (quote
+    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".ve")))
  '(linum-format " %7i ")
  '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
@@ -24,4 +28,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "misc" :family "dejavu sans mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "misc" :family "dejavu sans mono"))))
+ '(ediff-current-diff-A ((t (:background "#553333" :foreground "white"))))
+ '(ediff-current-diff-B ((t (:background "#335533" :foreground "white"))))
+ '(ediff-even-diff-A ((t (:background "light grey" :foreground "black"))))
+ '(ediff-even-diff-B ((t (:background "Grey" :foreground "black"))))
+ '(ediff-odd-diff-B ((t (:background "light grey" :foreground "black")))))
