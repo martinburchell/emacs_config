18a933b2331a12029b398c90543ecb7beeada02a
Martin Burchell <martinburchell@gmail.com>
2022-10-24 16:41:28 +0100

    Disable all that C++ CCLS/LSP stuff for now

--- .emacs.d/settings/c++-settings.el
+++ .emacs.d/settings/c++-settings.el
@@ -1,10 +1,10 @@
 (defun my-cpp-setup ()
-  (require 'ccls)
-  (setq ccls-executable "ccls")
-  (setq lsp-prefer-flymake nil)
-  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  ;(require 'ccls)
+  ;(setq ccls-executable "ccls")
+  ;(setq lsp-prefer-flymake nil)
+  ;(setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
   (setq c-default-style "k&r" c-basic-offset 4)
-  (lsp)
+  ;(lsp)
 )
 
 
